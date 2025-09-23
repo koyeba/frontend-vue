96bc676e6739af5f85a4656664d8af1c10608d0c
Albert <albert.tumba@gmail.com>
2025-09-23 23:34:41 +0200

    fix(grid): implement responsive grid layout for restaurant cards

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,22 +7,8 @@ const { data: restaurants, isError } = useFetchRestaurants();
 <template>
   <div>
     <RatingFilter />
-    <VAlert type="warning" class="my-4">
-      TODO: this should be presented as a grid<br>
-      • 3 columns max<br>
-      • 1 column on small devices <br>
-      • DO NOT USE
-      <a
-        href="https://vuetifyjs.com/en/components/grids/"
-        target="_blank"
-        class="text-indigo-700"
-        rel="noopener noreferrer"
-      >
-        Vuetify's grid components
-      </a>
-    </VAlert>
     <LoadingError v-if="isError" />
-    <div v-else-if="restaurants">
+    <div v-else-if="restaurants" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-4">
       <RestaurantCard
         v-for="restaurant of restaurants"
         :key="restaurant.id"
